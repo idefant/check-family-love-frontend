e36856052cc9ef8347d72fb32aadde1c6ea7f7c8
idefant <inik3004@gmail.com>
2022-11-05 12:28:09 +0500

    add Result page

---
+++ components/Icons/index.ts
@@ -0,0 +1,7 @@
+import * as Icons from './Icons';
+
+const Icon = {
+  Copy: Icons.CopyIcon,
+};
+
+export default Icon;
---
+++ pages/result/[id].tsx
@@ -0,0 +1,71 @@
+import classNames from 'classnames';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import Button from '../../components/Button';
+import Container from '../../components/Container';
+import Icon from '../../components/Icons';
+import Layout from '../../components/Layout';
+import SectionTitle from '../../components/SectionTitle';
+import loveHandsImg from '../../public/img/love_hands.png';
+import style from '../../styles/Result.module.scss';
+
+const Result = () => (
+  <Layout>
+    <SectionTitle>Результат анализа</SectionTitle>
+    <section className={style.section}>
+      <Container>
+        <h2 className={style.title}>Совместимость пары:</h2>
+
+        <div className={style.compatibility}>
+          <span className={style.compatibilityLabel}>как деловых отношений</span>
+          <span className={style.compatibilityDash}>–</span>
+          <span className={style.compatibilityPercent}>100 %</span>
+        </div>
+
+        <div className={style.compatibility}>
+          <span className={style.compatibilityLabel}>как семейных отношений</span>
+          <span className={style.compatibilityDash}>–</span>
+          <span className={style.compatibilityPercent}>1 %</span>
+        </div>
+
+        <div className={style.characters}>
+          <div className={classNames(style.card, style.cardMan)}>
+            <div className={style.cardTitle}>Характеристика мужчины:</div>
+            <div className={style.cardBody}>
+              <div>Тип личности: истерический</div>
+              <div>Тип характера: шизоидный</div>
+            </div>
+          </div>
+
+          <Image src={loveHandsImg} alt="Совместимость" />
+
+          <div className={classNames(style.card, style.cardWomen)}>
+            <div className={style.cardTitle}>Характеристика женщины:</div>
+            <div className={style.cardBody}>
+              <div>Тип личности: истерический</div>
+              <div>Тип характера: шизоидный</div>
+            </div>
+          </div>
+        </div>
+
+        <Button className={style.copyButton}>
+          <Icon.Copy />
+          Скопировать ссылку
+        </Button>
+
+        <div className={style.copyExplanation}>
+          Вы можете скопировать ссылку, нажав на кнопку выше, чтобы поделиться со своим партнером
+        </div>
+
+        <Link href="/" className={style.goMainLink}>
+          <Button theme="pink" outline className={style.goMainButton}>
+            Вернуться на главную страницу
+          </Button>
+        </Link>
+      </Container>
+    </section>
+  </Layout>
+);
+
+export default Result;
