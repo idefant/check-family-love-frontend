b797579d5919ade757a6b5f14ed75939f8da0e02
idefant <inik3004@gmail.com>
2022-11-17 13:55:33 +0500

    adapt styles on result page

--- pages/result/[id].tsx
+++ pages/result/[id].tsx
@@ -7,6 +7,7 @@ import Button from '../../components/Button';
 import Container from '../../components/Container';
 import Icon from '../../components/Icons';
 import Layout from '../../components/Layout';
+import Row from '../../components/Row';
 import SectionTitle from '../../components/SectionTitle';
 import useResettableState from '../../hooks/useResettableState';
 import loveHandsImg from '../../public/img/love_hands.png';
@@ -29,54 +30,59 @@ const Result = () => {
       <SectionTitle>Результат анализа</SectionTitle>
       <section className={style.section}>
         <Container>
-          <h2 className={style.title}>Совместимость пары:</h2>
+          <Row>
+            <div className={style.content}>
+              <h2 className={style.title}>Совместимость пары:</h2>
 
-          <div className={style.compatibility}>
-            <span className={style.compatibilityLabel}>в деловых отношениях</span>
-            <span className={style.compatibilityDash}>–</span>
-            <span className={style.compatibilityPercent}>100 %</span>
-          </div>
-
-          <div className={style.compatibility}>
-            <span className={style.compatibilityLabel}>в семейных отношениях</span>
-            <span className={style.compatibilityDash}>–</span>
-            <span className={style.compatibilityPercent}>1 %</span>
-          </div>
-
-          <div className={style.characters}>
-            <div className={classNames(style.card, style.cardMan)}>
-              <div className={style.cardTitle}>Характеристика мужчины:</div>
-              <div className={style.cardBody}>
-                <div>Тип личности: истерический</div>
-                <div>Тип характера: шизоидный</div>
+              <div className={style.compatibility}>
+                <span className={style.compatibilityLabel}>в деловых отношениях</span>
+                <span className={style.compatibilityDash}>–</span>
+                <span className={style.compatibilityPercent}>100 %</span>
               </div>
-            </div>
 
-            <Image src={loveHandsImg} alt="Совместимость" />
-
-            <div className={classNames(style.card, style.cardWomen)}>
-              <div className={style.cardTitle}>Характеристика женщины:</div>
-              <div className={style.cardBody}>
-                <div>Тип личности: истерический</div>
-                <div>Тип характера: шизоидный</div>
+              <div className={style.compatibility}>
+                <span className={style.compatibilityLabel}>в семейных отношениях</span>
+                <span className={style.compatibilityDash}>–</span>
+                <span className={style.compatibilityPercent}>1 %</span>
               </div>
+
+              <div className={style.characters}>
+                <div className={classNames(style.card, style.cardMan)}>
+                  <div className={style.cardTitle}>Характеристика мужчины:</div>
+                  <div className={style.cardBody}>
+                    <div>Тип личности: истерический</div>
+                    <div>Тип характера: шизоидный</div>
+                  </div>
+                </div>
+
+                <Image src={loveHandsImg} alt="Совместимость" className={style.cardHandsImg} />
+
+                <div className={classNames(style.card, style.cardWomen)}>
+                  <div className={style.cardTitle}>Характеристика женщины:</div>
+                  <div className={style.cardBody}>
+                    <div>Тип личности: истерический</div>
+                    <div>Тип характера: шизоидный</div>
+                  </div>
+                </div>
+              </div>
+
+              <Button className={style.copyButton} onClick={copyUrl}>
+                <Icon.Copy />
+                {copyButtonText.value}
+              </Button>
+
+              <div className={style.copyExplanation}>
+                Вы можете скопировать ссылку, нажав на кнопку выше, чтобы поделиться со своим
+                партнером
+              </div>
+
+              <Link href="/" className={style.goMainLink}>
+                <Button theme="pink" outline className={style.goMainButton}>
+                  Вернуться на главную страницу
+                </Button>
+              </Link>
             </div>
-          </div>
-
-          <Button className={style.copyButton} onClick={copyUrl}>
-            <Icon.Copy />
-            {copyButtonText.value}
-          </Button>
-
-          <div className={style.copyExplanation}>
-            Вы можете скопировать ссылку, нажав на кнопку выше, чтобы поделиться со своим партнером
-          </div>
-
-          <Link href="/" className={style.goMainLink}>
-            <Button theme="pink" outline className={style.goMainButton}>
-              Вернуться на главную страницу
-            </Button>
-          </Link>
+          </Row>
         </Container>
       </section>
     </Layout>
