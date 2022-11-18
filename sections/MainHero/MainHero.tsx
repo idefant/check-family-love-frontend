c167d0f45b4855d09d00c2906d433afd64d2f6be
idefant <inik3004@gmail.com>
2022-11-18 18:15:04 +0500

    add gender icons to hero section

--- sections/MainHero/MainHero.tsx
+++ sections/MainHero/MainHero.tsx
@@ -1,20 +1,28 @@
+import Image from 'next/image';
 import { FC } from 'react';
 
 import Container from '../../components/Container';
+import femaleImg from '../../public/img/female.png';
+import maleImg from '../../public/img/male.png';
 
 import style from './MainHero.module.scss';
 
 const MainHero: FC = () => (
   <section className={style.section}>
     <Container>
-      <h1 className={style.title}>
-        Оценка продолжительности совместной жизни с вашим партнером
-      </h1>
-      <h2 className={style.subtitle}>
-        Автоматическое определение нейроной сетью типа личности, характера Вас и
-        вашего партнера, социометрических данных и оценка на основе статистики
-        браков/разводов.
-      </h2>
+      <div className={style.heroInner}>
+        <h1 className={style.title}>
+          Оценка продолжительности совместной жизни с вашим партнером
+        </h1>
+        <h2 className={style.subtitle}>
+          Автоматическое определение нейроной сетью типа личности, характера Вас и
+          вашего партнера, социометрических данных и оценка на основе статистики
+          браков/разводов.
+        </h2>
+
+        <Image src={femaleImg} alt="Female icon" className={style.femaleIcon} />
+        <Image src={maleImg} alt="Male icon" className={style.maleIcon} />
+      </div>
     </Container>
   </section>
 );
