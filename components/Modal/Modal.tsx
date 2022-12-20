ba37400aa77fd2ec22f06f33a9b32e5721348056
idefant <inik3004@gmail.com>
2022-12-20 15:57:58 +0500

    load images from WrongPhotosModal immediately

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,9 +1,9 @@
 import { FC, ReactNode } from 'react';
-import { createPortal } from 'react-dom';
 import { CSSTransition } from 'react-transition-group';
 
 import { TModal } from '../../hooks/useModal';
 import Icon from '../Icons';
+import Portal from '../Portal';
 
 import style from './Modal.module.scss';
 
@@ -29,33 +29,34 @@ const Modal: FC<ModalProps> = ({ title, children, modal }) => {
 
   if (typeof window !== 'object') return null;
 
-  return createPortal(
-    <CSSTransition
-      in={isOpen}
-      timeout={150}
-      mountOnEnter
-      unmountOnExit
-      classNames={{
-        enterActive: style.modalOpening,
-        enterDone: style.modalOpened,
-        exitActive: style.modalExiting,
-      }}
-      onEnter={hiddenScroll}
-      onExited={showScroll}
-    >
-      <div className={style.wrapper} onClick={close}>
-        <div className={style.body} onClick={(e) => e.stopPropagation()}>
-          <div className={style.title}>
-            {title}
-            <button type="button" className={style.closeButton} onClick={close}>
-              <Icon.X />
-            </button>
+  return (
+    <Portal>
+      <CSSTransition
+        in={isOpen}
+        timeout={150}
+        classNames={{
+          enter: style.modalOpenStart,
+          enterActive: style.modalOpening,
+          enterDone: style.modalOpened,
+          exit: style.modalExitStart,
+          exitActive: style.modalExiting,
+        }}
+        onEnter={hiddenScroll}
+        onExited={showScroll}
+      >
+        <div className={style.wrapper} onClick={close}>
+          <div className={style.body} onClick={(e) => e.stopPropagation()}>
+            <div className={style.title}>
+              {title}
+              <button type="button" className={style.closeButton} onClick={close}>
+                <Icon.X />
+              </button>
+            </div>
+            <div className={style.content}>{children}</div>
           </div>
-          <div className={style.content}>{children}</div>
         </div>
-      </div>
-    </CSSTransition>,
-    document.body,
+      </CSSTransition>
+    </Portal>
   );
 };
 
